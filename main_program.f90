dec3ba0baa71aac4b9da0a34c38e8ae32249c434
TomoriNao <mengleshan23@mails.ucas.ac.cn>
2024-06-05 17:40:29 +0800

    class

--- main_program.f90
+++ main_program.f90
@@ -2,27 +2,21 @@ program main
   use my_module
   implicit none
 
-  type(Point) :: p(9)
+  type(Point) :: p(10)
   integer :: i
 
   ! Test for Tangent Linear Model
   print *, 'Test for Tangent Linear Model'
-  do i = 2, 10
-    call p(i-1)%initialize(i)
-    call p(i-1)%compute_ww()
-    call p(i-1)%compute_wwplus()
-    call p(i-1)%tangent_linear_model()
-    write(*, '(A, E8.2, A, F18.15)') 'dx = dy = ', p(i-1)%dxx, '   Ratio = ', (p(i-1)%wplus - p(i-1)%w) / p(i-1)%dw
+  do i = 1, 10
+    call p(i)%run_all(i+1)
+    write(*, '(A, E8.2, A, F18.15)') 'dx = dy = ', p(i)%dxx, '   Ratio = ', (p(i)%wplus - p(i)%w) / p(i)%dw
   end do
   
   ! Test for Adjoint Model
   print *, ' '
   print *, 'Test for Adjoint Model'
-  i = 9
-  call p(i-1)%tangent_linear_model()
-  print *,p(i-1)%dw
-  write(*, '(A, E21.15)') 'dw*dw         = ', p(i-1)%dw * p(i-1)%dw 
-  call p(i-1)%adjoint_model()
-  write(*, '(A, E21.15)') 'dxx*dx+dyy*dy = ', abs(p(i-1)%dxx * p(i-1)%dx + p(i-1)%dyy * p(i-1)%dy)
+  i = 1
+  write(*, '(A, E21.15)') 'dw*dw         = ', p(i)%dw ** 2
+  write(*, '(A, E21.15)') 'dxx*dx+dyy*dy = ', p(i)%dxx * p(i)%dx + p(i)%dyy * p(i)%dy
 
 end program main
--- my_module.f90
+++ my_module.f90
@@ -1,22 +1,25 @@
 module my_module
   implicit none
+  private
+  public :: Point
 
   ! Define the derived type
   type :: Point
     real*8 :: x0, y0, x, y, dx, dy, dxx, dyy, w, dw, wplus
   contains
-    procedure :: initialize => initialize_values
-    procedure :: compute_ww => ww
-    procedure :: compute_wwplus => ww_plus
-    procedure :: tangent_linear_model => intp2tgl
-    procedure :: adjoint_model => intp2adj
+    procedure ::  initialize_values
+    procedure ::  ww
+    procedure ::  ww_plus
+    procedure ::  intp2tgl
+    procedure ::  intp2adj
+    procedure ::  run_all
   end type Point
 
 contains
   ! Set test points and reference points
   subroutine initialize_values(this, i)
     class(Point), intent(inout) :: this
-    integer, optional, intent(in) :: i
+    integer, intent(in) :: i
 
     this%x0 = 0
     this%y0 = 0
@@ -60,9 +63,20 @@ contains
     class(Point), intent(inout) :: this
     real*8 :: dww
 
-    dww = this%w * this%dw
+    dww = -this%w * this%dw
     this%dx = 2 * (this%x - this%x0) * dww
     this%dy = 2 * (this%y - this%y0) * dww
   end subroutine intp2adj
+
+  ! Run all the subroutines
+  subroutine run_all(this,i)
+    class(Point), intent(inout) :: this
+    integer, optional, intent(in) :: i
+    call this%initialize_values(i)
+    call this%ww()
+    call this%ww_plus()
+    call this%intp2tgl()
+    call this%intp2adj()
+  end subroutine run_all
 
 end module my_module
